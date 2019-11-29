e1dc9d0ba8c490df8d44a1fc7d86fca63c243183
Sandro Mesterheide <sandro.mesterheide@uni-muenster.de>
2019-11-29 08:48:48 +0100

    Change from Headless Chrome with CDP to Puppeteer. Use serverless for deployment at AWS

---
+++ handler.js
@@ -0,0 +1,51 @@
+const serverless = require('serverless-http');
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const compression = require('compression');
+
+const server = require('./lib/server');
+
+server.init({
+  rendererOptions: {
+    inject: { prerendered: true },
+    renderAfterDocumentEvent: 'custom-render-trigger',
+    //headless: false
+  }
+});
+server.onRequest = server.onRequest.bind(server);
+
+server.use(require('./lib/plugins/basicAuth'));
+//server.use(require('./lib/plugins/sendPrerenderHeader'));
+//server.use(require('./lib/plugins/removeScriptTags'));
+server.use(require('./lib/plugins/httpHeaders'));
+// postProcess
+server.use({
+  pageLoaded: (req, res, next) => {
+    if (req.prerender.content && req.prerender.renderType == 'html') {
+      req.prerender.content = req.prerender.content
+        .replace(/<script (.*?)>/g, '<script $1 defer>')
+        .replace('id="app"', 'id="app" data-server-rendered="true"');
+    }
+    next();
+  }
+})
+//server.use(require('prerender-aws-s3-cache'))
+server.use(require('./lib/plugins/awsS3Cache'))
+
+app.disable('x-powered-by');
+app.use(compression());
+
+app.get('*', server.onRequest);
+
+//dont check content-type and just always try to parse body as json
+app.post('*', bodyParser.json({ type: () => true }), server.onRequest);
+
+module.exports.render = serverless(app, {
+  request: function(req, event, context) {
+    // context.callbackWaitsForEmptyEventLoop = false;
+    // Pass Lambda event to Express request
+    req.event = event;
+  }
+});
